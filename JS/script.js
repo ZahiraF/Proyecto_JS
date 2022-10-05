2f804aad3e3066efcda1146e599dba05322d97e7
ZahiraF <fernandez.zahira@gmail.com>
2022-10-05 16:44:43 -0300

    resolver consigna 1 del tp

---
+++ JS/script.js
@@ -0,0 +1,27 @@
+// variables, let, constantes
+
+// VARIABLES
+var nombre = "Zahira";
+var apellido = "Fernandez";
+console.log(nombre);
+console.log(apellido);
+
+//LET
+
+let frutaFavorita = "frambuesa";
+console.log(frutaFavorita);
+
+//CONST
+
+const animal = "tigre";
+console.log(animal);
+
+//FUNCIONES CALCULOS MATEMÁTICOS
+
+// SUMA
+
+//RESTA
+
+//DIVISIÓN
+
+//MULTIPLICACIÓN
