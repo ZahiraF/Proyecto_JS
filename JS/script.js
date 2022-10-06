a6402a94851d37f7cb1877680fe01957df3ac7ce
ZahiraF <fernandez.zahira@gmail.com>
2022-10-06 09:27:00 -0300

    creo y llamo funciones de calculos matematicos

--- JS/script.js
+++ JS/script.js
@@ -18,10 +18,43 @@ console.log(animal);
 
 //FUNCIONES CALCULOS MATEMÁTICOS
 
+var num1 = 10;
+var num2 = 5;
 // SUMA
-
+function funcionSuma(num1, num2) {
+    var sumas = 0;
+    sumas = num1 + num2;
+    return sumas;
+}
 //RESTA
-
+function funcionResta(num1, num2) {
+    var restas;
+    restas = num1 - num2;
+    return restas;
+}
 //DIVISIÓN
-
+function funcionDivision(num1, num2) {
+    var divisiones;
+    divisiones = num1 / num2;
+    return divisiones
+}
 //MULTIPLICACIÓN
+function funcionMultiplicacion(num1, num2) {
+    var multiplicaciones;
+    multiplicaciones = num1 * num2;
+    return multiplicaciones;
+}
+//MÓDULO O RESTO
+function funcionModulo(num1, num2) {
+    var modulos;
+    modulos = num1 % num2;
+    return modulos;
+}
+
+
+
+console.log(funcionSuma(num1, num2));
+console.log(funcionResta(num1, num2));
+console.log(funcionDivision(num1, num2));
+console.log(funcionMultiplicacion(num1, num2));
+console.log(funcionModulo(num1, num2));
